567a0e8c37939982d826637dd014f4e984fffbf5
Woouis <luiz.f.barreto@hotmail.com>
2024-08-11 00:56:48 -0300

    att desafio

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-let nome = 'luiz'
+//DESAFIO DIO - 
 
-console.log(nome)
+let nome = 'Santoryu'
+let quantidadeExperiencia =  7004
+let nivel = ['Ferro', 'Bronze', 'Prata', 'Ouro', 'Platina', 'Ascendente', 'Imortal', 'Radiante']
+
+if(quantidadeExperiencia <1000)
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[0])
+else if(quantidadeExperiencia>=1001 && quantidadeExperiencia<=2000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[1])
+} else if(quantidadeExperiencia>=2001 && quantidadeExperiencia<=5000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[2])
+} else if(quantidadeExperiencia>=5001 && quantidadeExperiencia<=7000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[3])
+} else if(quantidadeExperiencia>=7001 && quantidadeExperiencia<=8000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[4])
+} else if(quantidadeExperiencia>=8001 && quantidadeExperiencia<=9000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[5])
+} else if(quantidadeExperiencia>=9001 && quantidadeExperiencia<=10.000){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[6])
+} else if(quantidadeExperiencia>=10001){
+    console.log('0 herói de nome '+nome, 'está no nível de '+nivel[7])
+} else{
+    console.log('INDEFINIDO')
+}
